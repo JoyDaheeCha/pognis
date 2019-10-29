295272a54505863418949e623caf230ce8416b21
JoyDaheeCha <daheecha0612@gmail.com>
2019-10-29 16:51:11 +0900

    Add mock DB

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -1,4 +1,9 @@
-export const home = (req, res) => res.render("home");
+import { products } from "../db";
+
+export const home = (req, res) => {
+  res.render("home", { products });
+};
+
 export const join = (req, res) => res.render("join");
 export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.render("logout");
---
+++ db.js
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    id: 1234,
+    name: "샤카",
+    originalPrice: 100000,
+    currentPrice: 30000,
+    thumbUrl: "https://images.unsplash.com/photo-1531505487095-5fc508cce7b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  },
+  {
+    id: 1235,
+    name: "샤카1",
+    originalPrice: 100000,
+    currentPrice: 30000,
+    thumbUrl: "https://images.unsplash.com/photo-1531505487095-5fc508cce7b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  },
+  {
+    id: 1231,
+    name: "샤카2",
+    originalPrice: 100000,
+    currentPrice: 30000,
+    thumbUrl: "https://images.unsplash.com/photo-1531505487095-5fc508cce7b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  },
+  {
+    id: 1235,
+    name: "샤카1",
+    originalPrice: 100000,
+    currentPrice: 30000,
+    thumbUrl: "https://images.unsplash.com/photo-1531505487095-5fc508cce7b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  },
+  {
+    id: 1231,
+    name: "샤카2",
+    originalPrice: 100000,
+    currentPrice: 30000,
+    thumbUrl: "https://images.unsplash.com/photo-1531505487095-5fc508cce7b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  }
+];
