9e22c6ee897bb4bb77d294ec5e6894e0a70e885a
JoyDaheeCha <daheecha0612@gmail.com>
2019-11-01 13:15:58 +0900

    Add home, search function

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -1,10 +1,23 @@
-import { products } from "../db";
+import Product from "../models/Product";
 
-export const home = (req, res) => {
-  res.render("home", { products });
+export const home = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.render("home", { products });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { products: [] });
+  }
 };
 
 export const join = (req, res) => res.render("join");
 export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.render("logout");
-export const search = (req, res) => res.render("search");
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  const regExp = new RegExp(searchingBy, "i");
+  const products = Products.filter(product => regExp.test(product.name));
+  res.render("search", { searchingBy, products });
+};
