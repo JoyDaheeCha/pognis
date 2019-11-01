bc4982b75f636ca1d1b9515a7705057caf9720a9
JoyDaheeCha <daheecha0612@gmail.com>
2019-11-01 13:18:14 +0900

    Add multer for image url

--- middleware.js
+++ middleware.js
@@ -1,7 +1,12 @@
+import multer from "multer";
 import routes from "./routes";
+
+const multerProduct = multer({ dest: "uploads/" });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "포그니스";
   res.locals.routes = routes;
   next();
 };
+
+export const uploadImage = multerProduct.single("imageUrl");
